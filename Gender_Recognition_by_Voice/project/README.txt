0153ca314747802901f6c43f415cf78a2a9a2a18
树哥 <109694228@qq.com>
2018-09-13 12:15:29 +0800

    project commit

---
+++ Gender_Recognition_by_Voice/project/README.txt
@@ -0,0 +1,7 @@
+1. 项目中用到的库：sklearn、numpy、pandas、matplotlib
+2. notebook的运行时间：
+	DataExploration：约1分钟
+	Models：约25分钟，主要时间消耗在随机森林、svm和GDBT调参上GridSearchCV调SVM和GDBT都需要8分钟左右，建议不跑 "SVM(调参)" 和 "GDBT(调参)" 这两个格子。
+
+备注：
+Models.ipynb里的几个用GridSearchCV搜索参数的代码格子已注释
---
+++ Gender_Recognition_by_Voice/project/visuals.py
@@ -0,0 +1,84 @@
+###########################################
+# Suppress matplotlib user warnings
+# Necessary for newer version of matplotlib
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+#
+# Display inline matplotlib plots with IPython
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+###########################################
+
+import matplotlib.pyplot as pl
+import matplotlib.patches as mpatches
+import numpy as np
+import pandas as pd
+from time import time
+from sklearn.metrics import f1_score, accuracy_score
+
+
+def distribution(data, transformed = False):
+    """
+    Visualization code for displaying skewed distributions of features
+    """
+
+    # Create figure
+    fig = pl.figure(figsize = (11,5));
+
+    # Skewed feature plotting
+    # for i, feature in enumerate(data.columns):
+    for i, feature in enumerate(['skew', 'kurt']):
+        ax = fig.add_subplot(1, 2, i+1)
+        ax.hist(data[feature], bins = 25, color = '#00A0A0')
+        ax.set_title("'%s' Feature Distribution"%(feature), fontsize = 14)
+        ax.set_xlabel("Value")
+        ax.set_ylabel("Number of Records")
+        ax.set_ylim((0, 2000))
+        ax.set_yticks([0, 500, 1000, 1500, 2000])
+        ax.set_yticklabels([0, 500, 1000, 1500, ">2000"])
+
+    # Plot aesthetics
+    if transformed:
+        fig.suptitle("Log-transformed Distributions of Continuous Census Data Features", \
+            fontsize = 16, y = 1.03)
+    else:
+        fig.suptitle("Skewed Distributions of Continuous Census Data Features", \
+            fontsize = 16, y = 1.03)
+
+    fig.tight_layout()
+    fig.show()
+
+def pca_results(good_data, pca):
+    '''
+    Create a DataFrame of the PCA results
+    Includes dimension feature weights and explained variance
+    Visualizes the PCA results
+    '''
+
+    # Dimension indexing
+    dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1,len(pca.components_)+1)]
+
+    # PCA components
+    components = pd.DataFrame(np.round(pca.components_, 4), columns = good_data.keys())
+    components.index = dimensions
+
+    # PCA explained variance
+    ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+    variance_ratios = pd.DataFrame(np.round(ratios, 4), columns = ['Explained Variance'])
+    variance_ratios.index = dimensions
+
+    # Create a bar plot visualization
+    fig, ax = pl.subplots(figsize = (24,8))
+
+    # Plot the feature weights as a function of the components
+    components.plot(ax = ax, kind = 'bar');
+    ax.set_ylabel("Feature Weights")
+    ax.set_xticklabels(dimensions, rotation=0)
+
+
+    # Display the explained variance ratios
+    for i, ev in enumerate(pca.explained_variance_ratio_):
+        ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
+
+    # Return a concatenated DataFrame
+    return pd.concat([variance_ratios, components], axis = 1)
